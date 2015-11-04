1f416eddfc80d0bd3c23401d5b556be572c93b37
Aron Adler <aronadler@hotmail.com>
2015-11-04 14:26:46 +0000

    Basic working version

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var redis = require('redis');
+// var express = require('express');
+// var app = express();
+// var server = require('http').createServer(app);
+// var io = require('socket.io')(server);
+// var redis = require('redis');
 // var redisClient = redis.createClient();
+
+// io.on('connection', function(client) {
+// 	console.log('Client connected...');
+// 	client.emit('messages', {
+// 		hello: 'world'
+// 	});
+
+// 	client.on('save', function(data) {
+// 		console.log('Saved: ' + data.text);
+// 		client.emit('saved', data);
+// 	});
+// });
+
+
+// var storeMessage = function(name, data) {
+// 	var message = JSON.stringify({});
+
+// }
+
+// app.get('*', function(req, res) {
+// 	res.sendFile(__dirname + req.url);
+// });
+
+// server.listen(8080);
+
+
+
+////////////////////////////////
+// Streamlined Heroku deployment
+
+
+var express = require('express');
+var server = require('http').Server(app);
+var app = express();
+var io = require('socket.io')(server);
+
+
 
 io.on('connection', function(client) {
 	console.log('Client connected...');
@@ -19,21 +56,15 @@ io.on('connection', function(client) {
 
 
 
+app.set('port', (process.env.PORT || 5000));
 
-var storeMessage = function(name, data) {
-	var message = JSON.stringify({});
-
-
-
-	// 
-
-}
-
-
+app.use(express.static(__dirname + '/'));
 
 
 app.get('*', function(req, res) {
 	res.sendFile(__dirname + req.url);
 });
 
-server.listen(8080);
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));;
+});
