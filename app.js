4a767640f837a81d1ad4acb7afa012f40a6cae6e
Aron Adler <aronadler@hotmail.com>
2015-11-04 15:33:46 +0000

    Testing

--- app.js
+++ app.js
@@ -1,47 +1,9 @@
-// var express = require('express');
-// var app = express();
-// var server = require('http').createServer(app);
-// var io = require('socket.io')(server);
-// var redis = require('redis');
-// var redisClient = redis.createClient();
-
-// io.on('connection', function(client) {
-// 	console.log('Client connected...');
-// 	client.emit('messages', {
-// 		hello: 'world'
-// 	});
-
-// 	client.on('save', function(data) {
-// 		console.log('Saved: ' + data.text);
-// 		client.emit('saved', data);
-// 	});
-// });
-
-
-// var storeMessage = function(name, data) {
-// 	var message = JSON.stringify({});
-
-// }
-
-// app.get('*', function(req, res) {
-// 	res.sendFile(__dirname + req.url);
-// });
-
-
-// server.listen(8080);
-
-
-
-////////////////////////////////
-// Streamlined Heroku deployment
-
-
 var express = require('express');
-var server = require('http').Server(app);
 var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-console.log(app);
-
+var redis = require('redis');
+// var redisClient = redis.createClient();
 
 io.on('connection', function(client) {
 	console.log('Client connected...');
@@ -56,16 +18,75 @@ io.on('connection', function(client) {
 });
 
 
+var storeMessage = function(name, data) {
+	var message = JSON.stringify({});
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/'));
-
+}
 
 app.get('*', function(req, res) {
 	res.sendFile(__dirname + req.url);
 });
 
-server.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));;
-});
+
+server.listen(5000);
+
+
+
+////////////////////////////////
+// Streamlined Heroku deployment
+
+
+// var express = require('express');
+// var server = require('http').Server(app);
+// var app = express();
+// var io = require('socket.io')(server);
+
+
+
+// io.on('connection', function(client) {
+// 	console.log('Client connected...');
+// 	client.emit('messages', {
+// 		hello: 'world'
+// 	});
+
+// 	client.on('save', function(data) {
+// 		console.log('Saved: ' + data.text);
+// 		client.emit('saved', data);
+// 	});
+// });
+
+
+
+// app.set('port', (process.env.PORT || 5000));
+
+// app.get('*', function(req, res) {
+// 	res.sendFile(__dirname + req.url);
+// });
+
+// app.listen(app.get('port'), function() {
+// 	console.log('Node app is running on port', app.get('port'));
+// });
+
+
+
+
+
+
+// By the book
+
+// var app = require('express')();
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+
+// server.listen(80);
+
+// app.get('/', function (req, res) {
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
